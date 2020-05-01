04f29f7b9c0db6fb30e96548afcbf422a07449e6
JASPMobus <mobusj@live.com>
2020-05-01 10:45:27 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/users/new" => "users#new", as: "signup"
+  post "/users/create" => "users#create"
+  get "/hello" => "users#hello", as: "hello"
+
+  post "/sessions/create" => "sessions#create"
 end
