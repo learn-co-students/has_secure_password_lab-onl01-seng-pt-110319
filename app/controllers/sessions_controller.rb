57dd7e08dd5521893d0c52b469774fe408fe5952
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:46:48 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create 
+    @user = User.find_by(name: params[:user][:name])
+    authenticated = @user.try(:authenticate, params[:user][:password])
+    if authenticated
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      redirect_to :login
+    end
+  end
+  
+  
+end
