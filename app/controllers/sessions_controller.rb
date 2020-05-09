0ab7b97ae15c0832755e27e859a78d84a2f3b69c
nicksolony <nicksolony@gmail.com>
2020-05-09 17:31:42 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,26 @@ class SessionsController < ApplicationController
 
   def create
 #    binding.pry
-    @user = User.find_by(name: user_params[:name])
-    if @user.authenticate(user_params[:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if @user = User.find_by(name: user_params[:name])
+      if @user.authenticate(user_params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        wrong_info
+      end
     else
-      flash[:message] = "Wrong Username or password"
-      redirect_to login_path
+      wrong_info
     end
   end
 
   def destroy
     session.delete :user_id
+    redirect_to login_path
+  end
+
+  private
+  def wrong_info
+    flash[:message] = "Wrong Username or password"
     redirect_to login_path
   end
 end
